339d5e6c0534079437f56fd31399054d1d2bc734
Н <petrovnotbad@mail.ru>
2022-08-29 23:36:40 +0300

    first lesson commit exemple change code

--- class_work.py
+++ class_work.py
@@ -1,3 +1,59 @@
-print(2+3)
+# print(1+2)
+# Ctrl + ?
+# valve_1 = 4
+# valve_2 = 0.5
+# result =  valve_1 // valve_2
+# result = valve_1 / valve_2
+# result = valve_1 ** valve_2
+# print(result)
 
-print(2-3)
+# a = 2
+# b = 3
+# c = 4
+# D = b ** 2 - 4 * a * c
+
+
+# active_users_count = 1000
+# price = 10
+# total_money = active_users_count * price
+# # print(total_money)
+#
+#
+# # PascaCaseObjectNotification
+# # camelaCaseObjectNotification
+# # cebab-object-notification
+#
+# RATIO = 100
+# print(total_money, RATIO)
+
+# valve_1 = 4
+# valve_2 = 2
+# valve_type = type(valuve_2)
+# print(valve_2, valve_type)
+
+# valve = 12312313123
+#
+# new_valve = valve
+# new_valve = 123
+# print(id(valve), id(new_valve))
+
+# valve = "qwe"
+# # # print(valve, type(valve))
+# my_str = "qw"
+# #
+# result = valve * 5
+
+# result = ("123" + "5") * 2
+#
+# print(result)
+
+######### Литералы строк
+
+my_str = "New_string"
+my_str = 'New_string'
+my_str = '''New_string'''
+my_str = """New_string"""
+
+my_str = 'FC "Arsenal"'
+my_str = "I'm a boy"
+
